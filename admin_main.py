f697f366678d00134a99d29053a1471b3d8bacdb
Your Name <your-email@example.com>
2025-07-06 00:57:48 +0700

    update api

--- admin_main.py
+++ admin_main.py
@@ -2,6 +2,7 @@ from src.auth.admin.web_interface import create_admin_app
 import uvicorn
 
 app = create_admin_app(
+    database_url="postgresql://elevenlabs_auth_db_user:vVCP9zqfRfcFOD2OMHS8PJKxHpALAq07@dpg-d1kldjre5dus73enuikg-a/elevenlabs_auth_db",
     admin_username="admin",
     admin_password="admin123"
 )
